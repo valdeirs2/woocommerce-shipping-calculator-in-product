1689b0ca2ec11724de58a54a80d886aa6e60fc89
Valdeir <valdeirsantos92@gmail.com>
2018-05-18 23:40:36 -0300

    fix donation message

--- woocommerce-shipping-calculator-in-product.php
+++ woocommerce-shipping-calculator-in-product.php
@@ -38,7 +38,8 @@ if ( ! class_exists( __CLASS__ ) ) {
 
 		public function __construct() {
 
-			add_action( 'admin_notices', array( __CLASS__,'plugin_donate') );
+			if( isset( $_GET['page'] ) && $_GET['page'] == 'wc-settings' )
+				add_action( 'admin_notices', array( __CLASS__,'plugin_donate') );
 
 			add_action('wp', array($this,'init'));
 				
@@ -65,16 +66,16 @@ if ( ! class_exists( __CLASS__ ) ) {
 
 			$class = 'notice notice-info is-dismissible to apply';
 
-			$message = __( 'Está gostando de utilizar a Calculadora de Frete na Página do Produto? Me ajude a continuar criando novas soluçoes fazendo uma doação. <a href="https://pag.ae/bdv16S9" target="_blank"><img style="vertical-align: middle;margin-left: 15px;" src="https://stc.pagseguro.uol.com.br/public/img/botoes/doacoes/184x42-doar-roxo-assina.gif"></a>', '' );
+			$message = __( 'Está gostando de utilizar a Calculadora de Frete na Página do Produto? Me ajude a continuar criando novas soluções fazendo uma doação. <a href="https://pag.ae/bdv16S9" target="_blank"><img style="vertical-align: middle;margin-left: 15px;" src="https://stc.pagseguro.uol.com.br/public/img/botoes/doacoes/184x42-doar-roxo-assina.gif"></a>', '' );
 
 			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ),  $message ); 
 		}
 
 		public function Woo_Shipping_dependecy_notice() {
 
-			$class = 'notice notice-error';		
+			$class = 'notice notice-error is-dismissible';		
 
-			$message = __( 'Atenção! O plugin WC Correios é necessário para que WooCommerce Correios Shipping Calculator on Product funcione', '' );
+			$message = __( 'Atenção! O plugin WC Correios é necessário para que WooCommerce Correios Shipping Calculator in Product funcione', '' );
 
 			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
 		}
